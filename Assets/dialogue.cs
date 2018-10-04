46f2a8ec86b991d7b44ef205968d24336da56bef
Tetewet <theogorisse@gmail.com>
2018-10-04 13:48:04 -0400

    changements boîte de dialogue

---
+++ Assets/dialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogue : MonoBehaviour {
+
+    Text dialogueText;
+
+    void Start()
+    {
+        dialogueText = GetComponent<Text>();
+        dialogueText.text = "David + Brad + Roberto /n+ Alonso + Théo";
+        StartCoroutine(DialogueCoroutine());
+    }
+
+    IEnumerator DialogueCoroutine()
+    {
+        int number = 10;
+        dialogueText.text = "Foo";
+        dialogueText.text += " Bar";
+        dialogueText.text = "n = " + number;
+        yield return new WaitForSeconds(2);
+        dialogueText.text = "n = " + number;
+        number *= 20;
+    }
+}
