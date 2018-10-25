b5eeb29bcdb7f77bde541663175031e4ab618676
Alonso Bolarte <ugoalonso@hotmail.com>
2018-10-25 14:48:10 -0400

    QOL stuff

---
+++ Assets/Scripts/ScriptManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+    class ScriptManager:MonoBehaviour
+    {
+
+    [SerializeField] private Skill[] skillsToOrganize;
+    }
+
--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -2,66 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 
-
-    public class Skill
+public class Skill : MonoBehaviour
+{
+    public enum TargetType
     {
-        public enum TargetType
-        {
-            Self = 0,
-            AnAlly = 1,
-            OneEnemy = 2,
-            Enemy = 3,
-            Anyone = 4
-        }
-
-        static Random SkillRandom = new Random();
-        public string Name = "";
-        public DamageType Type;
-        public int Reach = 1;
-        //What percentage of the stats it uses; 1 = 100%, .2 = 20% of STR or INT - A la pokemon
-        public float Damage = 1;
-        protected float BaseCritChance = 5;
-        public TargetType Targets;
-
-
-        //Requirement    
-        public float MpCost = 0, HpCost = 0;
-        public int SpCost = 0;
-        public static Skill Base
-        {
-
-            get
-            {
-                var e = new Skill();
-                e.Name = "Attack";
-                e.SpCost = 2;
-                e.Reach = 1;
-                e.Type = DamageType.Physical;
-                e.Damage = .5f;
-                e.Targets = TargetType.OneEnemy;
-                return e;
-            }
-        }
-        public virtual void Activate(Actor Target, stat Stats = new stat(), Actor f = null)
-        {
-
-            var x = Damage;
-            if (Type == DamageType.Magical) x *= Stats.INT;
-            else if (Type == DamageType.Physical) x *= Stats.STR;
-
-            if ((Stats.LUC * 2 + BaseCritChance) > SkillRandom.Next(0, 100)) Damage *= 1.50f;
-
-            Target.TakeDamage(x, this, f);
-        }
-        public void Activate(Actor[] a, Actor f = null)
-        {
-            foreach (var item in a) { Activate(item); }
-        }
-
-
-
-
-
+        Self = 0,
+        AnAlly = 1,
+        OneEnemy = 2,
+        Enemy = 3,
+        Anyone = 4
     }
+
+
+    [SerializeField] private new string name = "";
+    public string Name { get { return name; } set { name = value; } }
+    [SerializeField] private DamageType type;
+    public DamageType Type { get { return type; } set { type = value; } }
+    [SerializeField] private int reach = 1;
+    public int Reach { get { return reach; } set {reach  = value; } }
+    //What percentage of the stats it uses; 1 = 100%, .2 = 20% of STR or INT - A la pokemon
+    [SerializeField] private float damage = 1;
+    public float Damage{ get { return damage; } set { damage = value; } }
+    [SerializeField] private float baseCritChance = 5;
+    public float BaseCritChance{ get { return baseCritChance; } set { baseCritChance = value; } }
+    [SerializeField] private TargetType targets;
+    public TargetType Targets { get { return targets; } set { targets = value; } }
+
+
+    //Requirement    
+    [SerializeField] private float mpCost = 0, hpCost = 0;
+    public float MpCost { get { return mpCost; } set { mpCost= value; } }
+    public float HpCost { get { return hpCost; } set { hpCost = value; } }
+    [SerializeField] private int spCost = 0;
+    public int SpCost { get { return spCost; } set { spCost = value; } }
+    public static Skill Base
+    {
+
+        get
+        {
+            var e = new Skill();
+            e.Name = "Attack";
+            e.SpCost = 2;
+            e.Reach = 1;
+            e.Type = DamageType.Physical;
+            e.Damage = .5f;
+            e.Targets = TargetType.OneEnemy;
+            return e;
+        }
+    }
+    public virtual void Activate(Actor target, Stat stats = new Stat(), Actor f = null)
+    {
+
+        var x = Damage;
+        if (Type == DamageType.Magical) x *= stats.INT;
+        else if (Type == DamageType.Physical) x *= stats.STR;
+
+        if ((stats.LUC * 2 + BaseCritChance) > UnityEngine.Random.Range(1, 101)) Damage *= 1.50f;
+
+        target.TakeDamage(x, this, f);
+    }
+    public void Activate(Actor[] a, Actor f = null)
+    {
+        foreach (var item in a) { Activate(item); }
+    }
+
+
+
+
+
+}
