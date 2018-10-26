dbaf6dce20e45458497a992b422e27be4c5e27c8
Unknown <bmele57@me.com>
2018-10-26 10:27:29 -0400

    FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

---
+++ Assets/dialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogue : MonoBehaviour {
+
+    Text dialogueText;
+
+    void Start()
+    {
+        dialogueText = GetComponent<Text>();
+        StartCoroutine(DialogueCoroutine());
+    }
+
+    IEnumerator DialogueCoroutine()
+    {
+        int number = 10;
+        dialogueText.text = "Foo";
+        yield return new WaitForSeconds(2);
+        dialogueText.text += " Bar";
+        yield return new WaitForSeconds(2);
+        dialogueText.text = "n = " + number;
+        number *= 20;
+        yield return new WaitForSeconds(2);
+        dialogueText.text = "n = " + number;
+    }
+}
