624740474951be85aab8a09fb5d142d8edfc8a65
Tetewet <theogorisse@gmail.com>
2018-11-01 14:36:35 -0400

    ZEMERGE added skill

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
- 
+using UnityEngine;
+
+
 public class Skill 
 {
     public enum TargetType
@@ -13,68 +15,40 @@ public class Skill
         Enemy = 3,
         Anyone = 4
     }
-    protected Weapon wep;
-    public bool Unlocked = false;
-    public string AnimationPath = "";
-
+    
     private new string name = "";
     public string Name { get { return name; } set { name = value; } }
-    private ProfessionType profession;
-    public ProfessionType Profession { get { return profession; } set { profession = value; } }
-    private DamageType type;
-    public DamageType  Type { get { return type; } set { type = value; } }
+    private Profession.ProfessionType profType;
+    public Profession.ProfessionType ProfType { get { return profType; } set { profType = value; } }
+    private DamageType dmgType;
+    public DamageType DmgType { get { return dmgType; } set { dmgType = value; } }
     private int reach = 1;
     public int Reach { get { return reach; } set {reach  = value; } }
-   
     //What percentage of the stats it uses; 1 = 100%, .2 = 20% of STR or INT - A la pokemon
     private float damage = 1;
     public float Damage{ get { return damage; } set { damage = value; } }
-
-
     private float baseCritChance = 5;
     public float BaseCritChance{ get { return baseCritChance; } set { baseCritChance = value; } }
-
     private TargetType targets;
     public TargetType Targets { get { return targets; } set { targets = value; } }
-
+    public bool Unlocked;
 
     //Requirement    
-    private float mpCost = 0, hpCost = 0;
-    public float MpCost { get { return mpCost; } set { mpCost= value; } }
-    public float HpCost { get { return hpCost; } set { hpCost = value; } }
-    private int spCost = 0, level = 0;
+    private int mpCost = 0, hpCost = 0, spCost = 0, level = 0;
+    public int MpCost { get { return mpCost; } set { mpCost= value; } }
+    public int HpCost { get { return hpCost; } set { hpCost = value; } }
     public int SpCost { get { return spCost; } set { spCost = value; } }
     public int Level { get { return level; } set { level = value; } }
-
-
-    public static Skill Weapon(Weapon w)
-    {
-        if (w == null) return Skill.Base;
-        else
-        {
-
-            var e = new Skill();
-            e.wep = w; 
-            e.name = "Attack";
-            e.SpCost = 2;
-            e.Reach = 1;
-            e.Type = w.DamageType;
-            e.baseCritChance = w.StatsBonus.CriticalHitFlat;
-            e.Damage = .5f;
-            e.Targets = TargetType.OneEnemy;
-            return e;
-        }
-    }
     public static Skill Base
     {
 
         get
         {
             var e = new Skill();
-            e.name = "Attack";
+            e.Name = "Attack";
             e.SpCost = 2;
             e.Reach = 1;
-            e.Type = DamageType.Melee;
+            e.DmgType = DamageType.Physical;
             e.Damage = .5f;
             e.Targets = TargetType.OneEnemy;
             return e;
@@ -84,12 +58,9 @@ public class Skill
     {
 
         var x = Damage;
+        if (DmgType == DamageType.Magical) x *= stats.INT;
+        else if (DmgType == DamageType.Physical) x *= stats.STR;
 
-       
-        if ( type == DamageType.Magic) x *= stats.INT;
-        if (type == DamageType.Physical || type == DamageType.Slashing || type == DamageType.Blunt) x *= stats.STR;
- 
-        if (wep != null) x += wep.ATK;
         if ((stats.LUC * 2 + BaseCritChance) > UnityEngine.Random.Range(1, 101)) Damage *= 1.50f;
 
         target.TakeDamage(x, this, f);
