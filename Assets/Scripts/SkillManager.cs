b10d8940490e86fe74400a7e1985db4129faf2c7
Alonso Bolarte <ugoalonso@hotmail.com>
2018-11-21 16:43:03 -0500

    Sqlite Mono Support

--- Assets/Scripts/SkillManager.cs
+++ Assets/Scripts/SkillManager.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Data.SQLite;
-
+using Mono.Data.Sqlite;
+using UnityEngine;
 
 static class SkillManager
 {
 
-    static void Start()
-    {
-      
-        
-    }
+   
 
     public static Skill[] GetSkillsByProfession(Actor actor)
     {
-        SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+    string conn = "URI=file:" + Application.dataPath + "/Database/Skills.db"; //Path to database.
+    IDbConnection dbconn;
+    dbconn = (IDbConnection)new SqliteConnection(conn);
+    dbconn.Open(); //Open connection to the database.
+    IDbCommand dbcmd = dbconn.CreateCommand();
+    string sqlQuery = "SELECT value,name, randomSequence " + "FROM PlaceSequence";
+    dbcmd.CommandText = sqlQuery;
+    IDataReader reader = dbcmd.ExecuteReader();
+   
+    reader.Close();
+    reader = null;
+    dbcmd.Dispose();
+    dbcmd = null;
+    dbconn.Close();
+    dbconn = null;
+    Skill[] skills = new Skill[1];
+        
 
+        return skills;
     }
 }
 
