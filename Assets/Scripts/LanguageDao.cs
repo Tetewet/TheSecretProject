c6040f8886ad72a0edbe4ad80f73c2c00dbd0406
Tetewet <theogorisse@gmail.com>
2018-12-16 09:41:52 -0500

    some dao

--- Assets/Scripts/LanguageDao.cs
+++ Assets/Scripts/LanguageDao.cs
@@ -11,21 +11,34 @@ public static class LanguageDao
 
     public static Dictionary<string, string> GetAll(string languageCode)
     {
+
         var languageDictionary = new Dictionary<string, string>();
         IDbConnection dbConnection = new SqliteConnection(DatabasePath);
 
         dbConnection.Open();
         IDbCommand dbCommand = dbConnection.CreateCommand();
-        dbCommand.CommandText = "SELECT name, " + languageCode + " FROM language";
+        dbCommand.CommandText = "SELECT name, " + languageCode + " FROM language"; //modifier la logique, pour pouvoir appeler la fonction de commande lors de l'appel de la fonction
         IDataReader dataReader = dbCommand.ExecuteReader();
 
-        while(dataReader.Read())
-        {
-            languageDictionary.Add(dataReader.GetString(0), dataReader.GetString(1));
-        }
+        //il faut rajouter une autre methode, qui va retourner le commandtext, et lorsqu'on appelle cette methode, on lui affiche le commandTexte qu'il faut
+
+        //dataHandler(dataReader);
+        //dataReader =>
+        //{
+
+            while (dataReader.Read())
+            {
+                languageDictionary.Add(dataReader.GetString(0), dataReader.GetString(1));
+            }
+        //});
 
         dataReader.Close();
         dbConnection.Close();
         return languageDictionary;
+    }
+
+    public static string ReadCommand(string enterCommand)
+    {
+        return enterCommand;
     }
 }
